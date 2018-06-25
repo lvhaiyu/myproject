4c962eb01215396deb430d00a91e11922cff582f
lvhaiyu <15144292188@163.com>
2018-06-25 23:10:39 +0800

    分类添加分类列表

---
+++ web2/myadmin/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import url
+
+from . views import views,userviews,typesviews
+
+urlpatterns = [
+  
+    url(r'^$',views.index,name='myadmin_index'),
+    #
+    url(r'^user/add/$',userviews.add,name='myadmin_user_add'),
+    url(r'^user/index/$',userviews.index,name='myadmin_user_list'),
+    url(r'^user/delete/$',userviews.delete,name='myadmin_user_delete'),
+    url(r'^user/edit/$',userviews.edit,name='myadmin_user_edit'),
+   
+
+    # 商品分类管理
+	url(r'^types/add/$', typesviews.add,name='myadmin_types_add'),
+	url(r'^types/index/$', typesviews.index,name='myadmin_types_list'),
+	url(r'^types/delete/$', typesviews.delete,name='myadmin_types_delete'),
+	# url(r'^types/edit/$', typesviews.edit,name='myadmin_types_edit'),
+
+    ]
